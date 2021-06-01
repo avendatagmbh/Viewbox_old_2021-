4b9cccc24c46b3918d971a78c87f748dfd52d60b
aakash-05 <83331499+aakash-05@users.noreply.github.com>
2021-06-01 13:22:13 +0530

    Add files via upload

---
+++ VersionUpdater/VersionUpdater/VersionUpdater/Options.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommandLineParser.Arguments;
+
+namespace VersionUpdater
+{
+    internal class Options
+    {
+        [ValueArgument(typeof(string), 'a', "gitapplicationpath", DefaultValue = "C:\\Program Files (x86)\\Git\\cmd\\", Description = "Set directory of GIT.exe. Default: C:\\Program Files (x86)\\Git\\cmd\\")]
+        public string GitApplicationPath { get; set; }
+
+        [ValueArgument(typeof(string), 'r', "gitrepopath", DefaultValue = "C:\\GIT\\", Description = "Set directory of GIT repository. Default: C:\\GIT\\")]
+        public string GitRepoPath { get; set; }
+
+        [ValueArgument(typeof(string), 'o', "originalfilepath", DefaultValue = "C:\\GIT\\AssemblyInfo_temp.cs", Description = "Set source file. Default: C:\\GIT\\AssemblyInfo_temp.cs")]
+        public string OriginalFilePath { get; set; }
+
+        [ValueArgument(typeof(string), 'd', "destinationfilepath", DefaultValue = "C:\\GIT\\AssemblyInfo.cs", Description = "Set source file. Default: C:\\GIT\\AssemblyInfo.cs")]
+        public string DestinationFilePath { get; set; }
+        
+    }
+}
---
+++ VersionUpdater/VersionUpdater/VersionUpdater/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace VersionUpdater
+{
+    class Program
+    {
+        private static Options _options;
+
+        static void Main(string[] args)
+        {
+            CommandLineParser.CommandLineParser parser = new CommandLineParser.CommandLineParser();
+            _options = new Options();
+            parser.ExtractArgumentAttributes(_options);
+            
+            try
+            {
+                parser.ParseCommandLine(args);
+
+                var pProcess = new System.Diagnostics.Process
+                {
+                    StartInfo =
+                    {
+                        FileName = "cmd.exe",
+                        Arguments = @"/C git.exe --git-dir=" + _options.GitRepoPath + @".git rev-list HEAD --count",
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        WorkingDirectory = _options.GitApplicationPath
+                    }
+                };
+                pProcess.Start();
+
+                var COUNT = int.Parse(pProcess.StandardOutput.ReadToEnd().Trim()).ToString();
+
+                var text = File.ReadAllText(_options.OriginalFilePath);
+                text = text.Replace(@"$WCREV$", COUNT.Trim());
+                for (var i = 1; i <= COUNT.Length; ++i)
+                {
+                    text = text.Replace(string.Format(@"$WCREV(U,{0})$", i.ToString()), COUNT.Substring(0, i));
+                    text = text.Replace(string.Format(@"$WCREV(L,{0})$", i.ToString()), COUNT.Substring(COUNT.Length - i, i));
+                }
+                File.WriteAllText(_options.DestinationFilePath, text);
+                pProcess.Close();
+            } catch (Exception e) {
+               throw new Exception(string.Format("Git is not installed or it is not configured in PATH as environment variable : {0}", e.Message));
+            }
+        }
+    }
+}
